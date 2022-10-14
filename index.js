a1c7e8d8f89954135926b05a8273370d044994a2
Abdulbari Qaisar <abdulbari.112917@gmail.com>
2022-10-14 23:01:00 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code in this file!
+let currentUser = "Hello" ;
+
+ let welcomeMessage = "Welcome to Flatbook, "
+
+ welcomeMessage=welcomeMessage+ currentUser +"!";
+
+ let  excitedWelcomeMessage = "WELCOME TO FLATBOOK, "
+ excitedWelcomeMessage = excitedWelcomeMessage + currentUser.toUpperCase() + "!"
+
+ let shortGreeting = "Welcome, ";
+
+ shortGreeting = shortGreeting + currentUser.charAt(0) + "!"
